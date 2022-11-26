edccb2a1f2b90e59c516ed12f1a2ae50858a9f90
rasoul707 <r.ahmadifar.1377@gmail.com>
2022-11-26 10:49:41 +0330

    fix banners problem

--- Modules/WebsiteSetting/Entities/Banner.php
+++ Modules/WebsiteSetting/Entities/Banner.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Modules\WebsiteSetting;
+namespace Modules\WebsiteSetting\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
